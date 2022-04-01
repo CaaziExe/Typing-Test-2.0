0acf2b4f17274d46ea6800a1b2ea05aab1371644
Caazi <97845969+CaaziExe@users.noreply.github.com>
2022-04-01 23:28:02 +0900

    Update dataSetup.js

--- script/dataSetup.js
+++ script/dataSetup.js
@@ -1,9 +1,9 @@
-let langugage = localStorage.getItem("lang");
-if(langugage == null){
-    langugage = "id";
+let language = localStorage.getItem("lang");
+if(language == null){
+    language = "id";
 }
 
-fetch(`source/word/${langugage}-words.txt`)
+fetch(`source/word/${language}-words.txt`)
 .then(r => r.text())
 .then(data => sessionStorage.setItem("words", data))
 
