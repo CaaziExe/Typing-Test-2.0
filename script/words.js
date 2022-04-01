3d46162f4dabc6e95ebc0c69130141c274db97dd
CaaziExe <caaziexe@gmail.com>
2022-04-01 22:55:24 +0900

    Update words.js

--- script/words.js
+++ script/words.js
@@ -1,5 +1,6 @@
 export function getRandomWord(){
     let data = sessionStorage.getItem("words").replaceAll("\r", "");
+    console.log(sessionStorage.getItem("words"));
     let listData = data.split("\n");
     return listData[Math.floor(Math.random() * (listData.length-1))];
 }
