a753b5169a02896eb72cffce2315a29371d0d23d
Caazi <97845969+CaaziExe@users.noreply.github.com>
2022-04-02 00:08:28 +0900

    Update words.js

--- script/words.js
+++ script/words.js
@@ -1,7 +1,6 @@
 export function getRandomWord(){
     try{
         let data = sessionStorage.getItem("words").replaceAll("\r", "");
-        console.log(sessionStorage.getItem("words"));
         let listData = data.split("\n");
         return listData[Math.floor(Math.random() * (listData.length-1))];
     }catch{
